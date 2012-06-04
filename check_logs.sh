6f199e3d9d1260c52843e1359ff46fdac7d19a68
shakalandro <shakalandro@gmail.com>
2012-06-04 13:52:35 -0700

    check script

---
+++ check_logs.sh
@@ -0,0 +1,8 @@
+# Check greg's walls
+diff3 storage/0/.messages_greg storage/2/.messages_greg storage/4/.messages_greg
+
+# Check jenny's walls
+diff3 storage/0/.messages_jenny storage/2/.messages_jenny storage/4/.messages_jenny
+
+# Check roy's walls
+diff3 storage/0/.messages_roy storage/2/.messages_roy storage/4/.messages_roy
