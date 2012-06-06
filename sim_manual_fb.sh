c6d5e60b5fccdda0348f5a6a5ba51ab93bb11cbc
Greg <gregbglw@gmail.com>
2012-06-06 13:22:13 -0700

    made script support failure rate args

--- sim_manual_fb.sh
+++ sim_manual_fb.sh
@@ -3,4 +3,26 @@
 rm -rf storage
 rm -f *.log
 rm -f *.replay
-./execute.pl -s -n FacebookNode -f 0 -L totallyOrderedLog -l partiallyOrderedLog 
+
+if [ "$1" == "" ]; then
+    A=0
+else
+    A=$1
+fi
+if [ "$2" == "" ]; then
+    B=0
+else
+    B=$2
+fi
+if [ "$3" == "" ]; then
+    C=0
+else
+    C=$3
+fi
+if [ "$4" == "" ]; then
+    D=0
+else
+    D=$4
+fi
+
+./execute.pl -s -n FacebookNode 0 -f 0 -L totallyOrderedLog -l partiallyOrderedLog -w $A -x $B -y $C -z $D
